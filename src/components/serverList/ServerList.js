6759aae594b11e3261f6a9f14448dc359c77026f
NathanPB <nathan.pbombana@gmail.com>
2019-07-23 02:02:13 -0300

    Breaking components into smaller ones.

---
+++ src/components/serverList/ServerList.js
@@ -0,0 +1,87 @@
+/*
+Rust Toolbox Copyright (C) 2019  Nathan P. Bombana, Lucas Vicari
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import Infinite from "react-infinite";
+import {CircularProgress} from "@material-ui/core";
+import {AutoSizer} from "react-virtualized";
+import * as React from "react";
+import ServerListNavigator from "../../serverList/ServerListNavigator";
+import ServerListItem from "./ServerListItem";
+
+export default function ServerList(props) {
+
+  const [ loading, setLoading ] = React.useState(false);
+  const [ servers, setServers ] = React.useState([]);
+  const sort = props.sort || 'rank';
+  const filter = props.filter || {};
+
+  const [ nextNavigator, setNextNavigator ] = React.useState(ServerListNavigator.createNavigator({sort: sort, ...filter}));
+
+  const buildElement = (server) =>
+    <ServerListItem
+      country={server.attributes.country}
+      name={server.attributes.name}
+      players={server.attributes.players}
+      maxPlayers={server.attributes.maxPlayers}
+      status={server.attributes.status}
+      latitude={server.attributes.location[1]}
+      map={server.attributes.details.map}
+      lastSeedChange={server.attributes.details.rust_last_seed_change}
+      modded={server.attributes.details.rust_modded}
+    />;
+
+  React.useEffect(() => {
+    if(loading) {
+      let newServers = servers;
+      nextNavigator.data().then(async data => {
+        newServers = newServers.concat(data.map(it => buildElement(it)));
+        setLoading(false);
+        setNextNavigator(await nextNavigator.next());
+        setServers(newServers);
+      });
+    }
+  });
+
+  const loadData = () => {
+    if(nextNavigator && nextNavigator.next){
+      setLoading(true);
+    }
+  };
+
+  return (
+    <AutoSizer>
+      {
+        ({ height, width }) => (
+          (height > 0 && width > 0) &&
+          <div style={{ width: width }} className="server-list">
+            <Infinite
+              containerHeight={height}
+              elementHeight={60}
+              infiniteLoadBeginEdgeOffset={128}
+              isInfiniteLoading={loading}
+              loadingSpinnerDelegate={<CircularProgress style={{ marginLeft: 'auto', marginRight: 'auto', display: 'block' }} />}
+              onInfiniteLoad={loadData}
+              timeScrollStateLastsForAfterUserScrolls={150}
+            >
+              {servers}
+            </Infinite>
+          </div>
+        )
+      }
+    </AutoSizer>
+  )
+}
--- src/components/views/ServerList.js
+++ src/components/views/ServerList.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Page from "./Page";
 import {
-  CircularProgress,
   Collapse,
   IconButton,
   Toolbar,
@@ -13,51 +12,46 @@ import {
   Menu, ArrowDownward,
   VerifiedUser
 } from "@material-ui/icons"
-import ServerListNavigator from "../../serverList/ServerListNavigator";
-import Infinite from 'react-infinite';
 import './ServerList.css';
 import Badge from "../Badge";
-import {AutoSizer} from "react-virtualized";
+import ServerListComponent from '../serverList/ServerList';
 
 export default class ServerList extends Page {
 
   constructor(props) {
     super(props, 'Server List');
-    this.state.options = {sort: 'rank', filter: {}};
-    this.state.servers = [];
-    this.state.isLoading = false;
+    this.state.sort = 'rank';
+    this.state.filter = {};
     this.state.showButtons = true;
 
-    this.loadData = this.loadData.bind(this);
-    this.buildElement = this.buildElement.bind(this);
     this.onFilter = this.onFilter.bind(this);
     this.onSort = this.onSort.bind(this);
-    this.rebuildNavigator = this.rebuildNavigator.bind(this);
-
-    this.rebuildNavigator();
   }
 
   onFilter = (filterObject) => {
-    this.state.options.search = filterObject;
+    this.setState({
+      filter: filterObject
+    });
   };
 
   onSort = (sortedBy) => {
-    this.state.options.filter = sortedBy
-  };
-
-  rebuildNavigator = () => {
-    this.state.nextNavigator = ServerListNavigator.createNavigator({sort: this.state.options.sort, ...this.state.options.filter});
+    this.setState({
+      sort: sortedBy
+    });
   };
 
   renderHeader = () => {
     return (
       <div style={{width: '100%', zIndex: 1}} className="themed-container">
         <Toolbar disableGutters={true}>
-          <IconButton style={{margin: '0.2em'}} align="left">
+          <IconButton
+            style={{margin: '0.2em'}}
+            align="left"
+            onClick={() => this.setState({toMenu: true})}
+          >
             <Menu
               style={{ color: 'white', fontSize: '1.5em' }}
               className="themed-container"
-              onClick={() => this.setState({toMenu: true})}
             />
           </IconButton>
           <div style={{flexGrow: 1}}>
@@ -103,102 +97,5 @@ export default class ServerList extends Page {
       )
   };
 
-  buildElement = (server) =>
-    <article className="infinite-list-item server-item">
-      <img src={`https://www.countryflags.io/${server.attributes.country}/flat/64.png`} alt={`flag ${server.attributes.country}`}/>
-      <section className="server-info">
-
-        <section style={{ display: 'flex', flexGrow: '100', maxWidth: '100%' }}>
-          <section className="server-title">
-            { false && /* TODO something to check if the server is a official one */ (
-              <span style={{ marginRight: '0.5em' }} title="Official Server">
-                <VerifiedUser style={{ fontSize: 12, color: 'gold'}}/>
-              </span>
-            )}
-            {server.attributes.name}
-          </section>
-          <section className="server-players">
-            {server.attributes.players}/{server.attributes.maxPlayers}
-          </section>
-        </section>
-
-        <section className="badges">
-          <Badge
-            display={server.attributes.status !== 'online'}
-            color="#D72439"
-            tooltip="Server Status"
-          >
-            {server.attributes.status}
-          </Badge>
-          <Badge
-            color="blue"
-            tooltip="Map Type"
-          >
-            {
-              //Select the icon according to the latitude of the physical server location
-              `${['🌎', '🌍', '🌏'][Math.round(server.attributes.location[1] / 128) + 1]} ${server.attributes.details.map}`
-            }
-          </Badge>
-          <Badge
-            color="blue"
-            tooltip="Map Age"
-          >
-            {
-              //Calculates the server map time based on the last change date
-              '📅 ' + ((+new Date() - +new Date(server.attributes.details.rust_last_seed_change)) / 86400000).toFixed(2)
-            } Days
-          </Badge>
-          <Badge
-            display={server.attributes.details.rust_modded}
-            color="blue"
-            tooltip="Modded Server"
-          >
-            <span> 🔧 Modded</span>
-          </Badge>
-        </section>
-
-      </section>
-    </article>;
-
-  loadData = () => {
-    console.log('aaa');
-    if(this.state.nextNavigator && this.state.nextNavigator.next){
-      this.setState({isLoading: true}, ()=> {
-        let servers = this.state.servers;
-        this.state.nextNavigator.data().then(async data => {
-          servers = servers.concat(data.map(it => this.buildElement(it)));
-          this.setState({
-            isLoading: false,
-            nextNavigator: await this.state.nextNavigator.next(),
-            servers: servers
-          });
-        });
-      })
-    }
-  };
-
-  drawnPage = () => {
-    return(
-      <AutoSizer>
-        {
-          ({ height, width }) => (
-            (height > 0 && width > 0) &&
-              <div style={{ width: width }} className="server-list">
-                <Infinite
-                  containerHeight={height}
-                  elementHeight={60}
-                  infiniteLoadBeginEdgeOffset={128}
-                  isInfiniteLoading={this.state.isLoading}
-                  loadingSpinnerDelegate={<CircularProgress style={{ marginLeft: 'auto', marginRight: 'auto', display: 'block' }} />}
-                  onInfiniteLoad={this.loadData}
-                  timeScrollStateLastsForAfterUserScrolls={150}
-                >
-                  {this.state.servers}
-                </Infinite>
-              </div>
-          )
-        }
-      </AutoSizer>
-    );
-  };
+  drawnPage = () => <ServerListComponent sort={this.state.sort} filter={this.state.filter}/>
 }
