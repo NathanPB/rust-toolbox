472ca1080d5f012b6590f080f5f72214e40fa5ad
NathanPB <nathan.pbombana@gmail.com>
2019-07-23 01:09:08 -0300

    Rework of the server list items to improve responsiveness

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,11 +1,18 @@
 import * as React from 'react';
+import './Badge.css';
 
 export default class Badge extends React.Component {
 
   render = () => {
     return(
-      (((this.props.display === undefined) || this.props.display ) && this.props.children) ?
-      <div title={this.props.tooltip} style={{ ...this.props.style, backgroundColor: this.props.color, borderRadius: 4, padding: 2, display: 'inline', color: 'white', marginRight: 4, textTransform: 'capitalize'}}>
+      ((( this.props.display === undefined) || this.props.display ) && this.props.children) ?
+      <div
+        title={this.props.tooltip}
+        className="Badge"
+        style={{
+          ...this.props.style,
+          backgroundColor: this.props.color,
+        }}>
         {this.props.children}
       </div> : null
     )
