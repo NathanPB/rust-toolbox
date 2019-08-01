NathanPB/rust-toolbox

commit 42f69bc76db4a7220e9d70489d3c3ea3ccb4f4c2

Oops I forgot to move a file :/

src/components/views/ServerList.js
@@ -12,7 +12,7 @@ import {
   Menu, ArrowDownward,
   VerifiedUser
 } from "@material-ui/icons"
-import './ServerList.css';
+import '../serverList/ServerListItem.css';
 import Badge from "../Badge";
 import ServerListComponent from '../serverList/ServerList';
 

commit b37f3d86eda29bbbe4d83b43a99d3fe60efb70d8

Fixed arrow not rotating, broken in 42f69bc76db4a7220e9d70489d3c3ea3ccb4f4c2

src/components/views/ServerList.js
@@ -89,7 +89,7 @@ export default class ServerList extends Page {
           >
             <ArrowDownward
               className={`${this.state.showButtons && 'arrow-reversed'}`}
-              style={{ color: 'white', fontSize: '1.5em', transition: 'all 0.5s' }}
+              style={{ color: 'white', fontSize: '1.5em', transition: 'all 0.5s', transform: `rotate(${this.state.showButtons ? 180 : 0}deg)` }}
             />
           </IconButton>
         </Toolbar>